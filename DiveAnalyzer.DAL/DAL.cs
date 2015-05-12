0fa86f6d56a1c4802baceeb10cab2751cd652079
emeshuris <edward@theemm.com>
2015-05-12 08:00:10 -0700

    Adding a DAl

---
+++ DiveAnalyzer.DAL/DAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace DiveAnalyzer.DAL
+{
+    public static class DAL
+    {
+        const string connectionString = "mongodb://@192.168.99.100:32768/";
+        public static void Test()
+        {
+            var client = new MongoClient(connectionString);
+
+            //Use the MongoClient to access the server
+           MongoDB.Driver.server MongoServer server = client.GetServer();
+
+            // Use the server to access the 'test' database
+            MongoDatabase database = server.GetDatabase("test");
+        }
+    }
+}
