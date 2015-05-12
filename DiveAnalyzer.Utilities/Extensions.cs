4ee7a4c160eaaa3f5713cf7ce5a8310fd77576c5
emeshuris <edward@theemm.com>
2015-05-11 21:17:43 -0700

    Renaming stuff

--- DiveAnalyzer.Utilities/Extensions.cs
+++ DiveAnalyzer.Utilities/Extensions.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DiveAnalyzer.Utilities
+﻿namespace DiveAnalizer.Utilities
 {
     //public static class Extensions
     //{
--- DiveAnalyzer.Utilities/Properties/AssemblyInfo.cs
+++ DiveAnalyzer.Utilities/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DiveAnalyzer.Utilities")]
+[assembly: AssemblyTitle("DiveAnalizer.Utilities")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DiveAnalyzer.Utilities")]
+[assembly: AssemblyProduct("DiveAnalizer.Utilities")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
