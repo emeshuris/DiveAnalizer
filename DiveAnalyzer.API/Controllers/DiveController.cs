8865c5743dee0d45ba36513d8091442f23f0a064
emeshuris <edward@theemm.com>
2015-05-17 22:16:19 -0700

    Fuck xamarin, configured api to use local iis instead of iisexpress.

---
+++ DiveAnalyzer.API/Controllers/DiveController.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace DiveAnalyzer.API.Controllers
+{
+    public class DiveController : ApiController
+    {
+        public async Task<IHttpActionResult> Get()
+        {
+            var Dives = await DAL.DAL.AsyncGetDives();
+
+            return Ok(Dives);
+        }
+
+        // GET: api/Dive/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Dive
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Dive/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Dive/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
